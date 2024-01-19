40c9bdb5cc1b8ff60f7648343474970293e15d8f
Ian Ellis <ellisiana@gmail.com>
2024-01-19 14:13:02 +0000

    Made similar to k8s_nexus set up

---
+++ install-uptime-kuma.sh
@@ -0,0 +1,60 @@
+unset USE_KIND
+# Check if kubectl is available in the system
+if kubectl 2>/dev/null >/dev/null; then
+  # Check if kubectl can communicate with a Kubernetes cluster
+  if kubectl get nodes 2>/dev/null >/dev/null; then
+    echo "Kubernetes cluster is available. Using existing cluster."
+    export USE_KIND=0
+  else
+    echo "Kubernetes cluster is not available. Creating a Kind cluster..."
+    export USE_KIND=X
+  fi
+else
+  echo "kubectl is not installed. Please install kubectl to interact with Kubernetes."
+  export USE_KIND=X
+fi
+
+if [ "X${USE_KIND}" == "XX" ]; then
+    # Make sure cluster exists if Mac
+    kind  get clusters 2>&1 | grep "kind-uptime-kuma"
+    if [ $? -gt 0 ]
+    then
+        envsubst < kind-config.yaml.template > kind-config.yaml
+        kind create cluster --config kind-config.yaml --name kind-uptime-kuma
+    fi
+
+    # Make sure create cluster succeeded
+    kind  get clusters 2>&1 | grep "kind-uptime-kuma"
+    if [ $? -gt 0 ]
+    then
+        echo "Creation of cluster failed. Aborting."
+        exit 666
+    fi
+fi
+
+# add metrics
+kubectl apply -f https://dev.ellisbs.co.uk/files/components.yaml
+
+# install local storage
+kubectl apply -f  local-storage-class.yml
+
+# create uptime-kuma namespace, if it doesn't exist
+kubectl get ns uptime-kuma 2> /dev/null
+if [ $? -eq 1 ]
+then
+    kubectl create namespace uptime-kuma
+fi
+
+# create deployment
+kubectl apply -f uptime-kuma-deployment.yml
+
+# sort out persistent volume
+if [ "X{$USE_KIND}" == "XX" ];then
+  export NODE_NAME=$(kubectl get nodes |grep control-plane|cut -d\  -f1|head -1)
+  envsubst < uptime-kuma.pv.kind.template > uptime-kuma.pv.yml
+else
+  export NODE_NAME=$(kubectl get nodes | grep -v ^NAME|grep -v control-plane|cut -d\  -f1|head -1)
+  envsubst < uptime-kuma.pv.linux.template > uptime-kuma.pv.yml
+  echo mkdir -p ${PWD}/uptime-kuma|ssh -o StrictHostKeyChecking=no ${NODE_NAME}
+fi
+kubectl apply -f uptime-kuma.pv.yml

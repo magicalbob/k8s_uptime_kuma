cdc1d531d84b4355433ad466fc73e7b9cd0be248
Ian Ellis <ellisiana@gmail.com>
2024-01-23 18:39:33 +0000

    Revert "added unit tests"
    This reverts commit 272cfd16cd84ccae314f77b877b143a7c77250c1.

--- testscript.sh
+++
@@ -1,6 +0,0 @@
-#cd /opt/pwd
-pip install -r requirements.txt
-pip install uptime_kuma_api
-~/.local/bin/coverage run -m unittest discover -v -s python -p '*_test.py'
-~/.local/bin/coverage xml
-rm -rf __pycache__

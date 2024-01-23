a9867c718744c486105c4fca35bb2a5de8aa4b02
Ian Ellis <ellisiana@gmail.com>
2024-01-23 12:10:34 +0000

    wait for username field

--- python/create_user.py
+++ python/create_user.py
@@ -20,18 +20,22 @@ url = f"http://0.0.0.0:3001/"
 browser.get(url)
 
 print(" Find the username, password, and repeat password fields")
-username_field = browser.find_element(By.ID, "floatingInput")
+wait = WebDriverWait(browser, 10)  # Wait for up to 10 seconds
+username_field = wait.until(EC.presence_of_element_located((By.ID, "floatingInput")))
 password_field = browser.find_element(By.ID, "floatingPassword")
-repeat_password_field = browser.find_element(By.ID, "repeat")
-
-print(" Fill in the fields")
-username_field.send_keys(os.environ.get("UPTIME_USERNAME"))
-password_field.send_keys(os.environ.get("UPTIME_PASSWORD"))
-repeat_password_field.send_keys(os.environ.get("UPTIME_PASSWORD"))
-
-print(" Find and click the Create button")
-create_button = browser.find_element(By.XPATH, '//button[@data-cy="submit-setup-form"]')
-create_button.click()
+try:
+  repeat_password_field = browser.find_element(By.ID, "repeat")
+  
+  print(" Fill in the fields")
+  username_field.send_keys(os.environ.get("UPTIME_USERNAME"))
+  password_field.send_keys(os.environ.get("UPTIME_PASSWORD"))
+  repeat_password_field.send_keys(os.environ.get("UPTIME_PASSWORD"))
+  
+  print(" Find and click the Create button")
+  create_button = browser.find_element(By.XPATH, '//button[@data-cy="submit-setup-form"]')
+  create_button.click()
+except:
+  pass
 
 print(" Close the WebDriver")
 browser.quit()
